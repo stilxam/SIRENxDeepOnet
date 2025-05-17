58823d2104dfb75811ca9d08f5a144bb504be760
Maxwell Litsios <79113615+stilxam@users.noreply.github.com>
2025-05-17 23:00:23 +0200

    Add files via upload

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "JAX + CUDA development shell using flakes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            cudaSupport = true;
+          };
+        };
+        pythonPackages = pkgs.python312Packages;
+
+        python = pkgs.python312.withPackages (ps: with ps; [
+          jax
+          jaxlib
+	  jaxtyping
+          #wandb
+          equinox
+          optax
+          notebook
+	  tqdm
+	  matplotlib
+	  seaborn
+	  dash
+	  scikit-learn
+          #blackjax
+        ]);
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            #pkgs.cudatoolkit
+            python
+          ];
+        };
+      });
+}
