94ce2b3cee0c7f1c0f4bd29fa2e00bb48913f3c6
installer <installer@gmail.com>
2025-06-24 16:23:30 +0200

    Add dashboard and preprocessing scripts for feature engineering and data visualization

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "JAX + CUDA development shell using flakes";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -31,6 +32,8 @@
 	  seaborn
 	  dash
 	  scikit-learn
+	  pandas
+	  pandas-stubs
           #blackjax
         ]);
       in {
